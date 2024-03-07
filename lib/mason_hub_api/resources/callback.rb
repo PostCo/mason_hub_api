3179cb1ba96d02e23de988b33e2de40dea8d33bc
Andy Chong <andygg1996personal@gmail.com>
2024-03-07 14:46:44 +0800

    Add DELETE to Callback resource

--- lib/mason_hub_api/resources/callback.rb
+++ lib/mason_hub_api/resources/callback.rb
@@ -2,6 +2,12 @@
 
 module MasonHubAPI
   class CallbackResource < Resource
+    def all(message_type: "all")
+      response = get_request("callbacks", params: { message_type: message_type })
+
+      response.body.map { |attributes| Callback.new(attributes) }
+    end
+
     def create(attributes)
       # accept either a single hash or an array of hashes
       attributes = [attributes] if attributes.is_a?(Hash)
@@ -11,10 +17,19 @@ module MasonHubAPI
       response
     end
 
-    def all(message_type: "all")
-      response = get_request("callbacks", params: { message_type: message_type })
+    #
+    # Delete the registered callbacks
+    #
+    # @param [Array] message_types An array of message types to delete.
+    # Available type: "skuInventoryChange", "orderEvent", "orderCancelResolution", "orderUpdateResolution", "rmaEvent", "inboundShipmentEvent", "snapshotReady"
+    #
+    # @return [Response] An array of deleted callbacks
+    #
+    def delete(message_types)
+      message_types = [message_types] if message_types.is_a?(String)
+      raise ArgumentError, "message_types must be an array or string" unless message_types.is_a?(Array)
 
-      response.body.map { |attributes| Callback.new(attributes) }
+      delete_request("callbacks", body: message_types).body
     end
   end
 end
